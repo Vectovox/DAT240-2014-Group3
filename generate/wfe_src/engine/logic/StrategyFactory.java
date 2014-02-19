5ccb791636594381b66a7b43a4e9a7e48733bcbe
Vectovox <robert-kaufmann@live.com>
2014-02-19 11:44:52 +0100

    Added interface and updated factory.

--- generate/wfe_src/engine/logic/StrategyFactory.java
+++ generate/wfe_src/engine/logic/StrategyFactory.java
@@ -1,10 +1,10 @@
 package engine.logic;
 
-public class StrategyFactory {
+public final class StrategyFactory {
 	
-	private StrategyFactory() {
-		
+	private StrategyFactory() {}
+	
+	public IOperationStrategy CreateStrategy(Class type, String name){
+		return null; // TODO: Make strategies depending on the input
 	}
-	
-	// TODO: Implement factory method...
 }
